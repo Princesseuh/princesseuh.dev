86a9049d83f7e374fcd6f5945e963b0d2fb32494
Princesseuh <princssdev@gmail.com>
2016-12-06 16:37:28 -0500

    Oops. We're supposed to be using https!

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://princesseuh.net/'
+SITEURL = 'https://princesseuh.net'
 RELATIVE_URLS = False
 
 FEED_ATOM = 'blog/feeds/atom.xml'
